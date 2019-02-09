af78a35ed4842969e405b0cea998f98ebfdb9e23
Tomanek1 <Tomanek1@seznam.cz>
2019-02-09 14:05:29 +0100

    Přejmenování
    Přejmenování hlavní knihovny na SqlFileQuery.Lib

--- SqlFileQuery.Lib/SqlFileQuery.cs
+++ SqlFileQuery.Lib/SqlFileQuery.cs
@@ -8,9 +8,9 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
+using SqlFileQuery.Tools;
 
-namespace SqlFileQuery
-
+namespace SqlFileQuery.Lib
 {
     /// <summary>
     /// Třída pro přímý přístup k SQL Databázi.
--- SqlFileQuery.Lib/Tools/SqlDataTypeMapper.cs
+++ SqlFileQuery.Lib/Tools/SqlDataTypeMapper.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Runtime.CompilerServices;
 using System.Text;
 
-namespace SqlFileQuery
+namespace SqlFileQuery.Tools
 {
     public static class SqlDataTypeMapper
     {
