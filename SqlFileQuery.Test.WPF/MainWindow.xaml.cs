6d9a9b9a0ea3d7f37c1d5a8ca24ff604ddc725c5
Petr <Tomanek1@seznam.cz>
2019-02-20 14:15:12 +0100

    Přidáno získání volajícího assembly

--- SqlFileQuery.Test.WPF/MainWindow.xaml.cs
+++ SqlFileQuery.Test.WPF/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SqlFileQuery.Test.WPF;
 
 namespace SqlFileQuery.Test.WPF
 {
@@ -20,9 +21,28 @@ namespace SqlFileQuery.Test.WPF
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		SqlDirect sqlDirect = new SqlDirect();
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				var n = sqlDirect.GetActivePublishers();
+			}
+			catch (Exception ex)
+			{
+				string exx = ex.ToString();
+				throw;
+			}
+		}
+
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+			var n = sqlDirect.Get2();
 		}
 	}
 }
