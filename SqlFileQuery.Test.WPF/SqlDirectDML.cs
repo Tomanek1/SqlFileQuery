dff7c340d82c80fc83675d3012753e1665aa3717
Tomanek1 <Tomanek1@seznam.cz>
2019-02-23 19:25:03 +0100

    Přidání DML testovacích souborů a vytvoření testovací struktury

---
+++ SqlFileQuery.Test.WPF/SqlDirectDML.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace SqlFileQueryLib.Test.WPF
+{
+	public class SqlDirectDML
+	{
+		SqlFileQuery sqlFileQuery = null;
+
+		internal SqlDirectDML()
+		{
+			string con = ConfigurationManager.ConnectionStrings[1].ConnectionString;
+			this.sqlFileQuery = new SqlFileQuery(new SqlConnection(con), "SqlScripts.BasicQueries");
+		}
+
+		internal object SelectScalar()
+		{
+			SqlCommand cmd = sqlFileQuery.CreateCommand("GetActivePublishers.sql");
+			sqlFileQuery.AddMultipleParameters(cmd, new[] { new { a = 1 }, new { a = 2 } });
+			throw new NotImplementedException();
+		}
+
+		internal object SelectTable()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal object SelectGrid()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal object Delete()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal object Update()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal object InsertMultiple()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal object InsertSingle()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
